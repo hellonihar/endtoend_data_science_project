7b78fa73ce03c1ba689ae5bc40c3c61e3a834918
Nihar Ranjan <hellonihar@hotmail.com>
2026-01-11 21:26:23 +0530

    final commit after project structure creation automated

---
+++ template.py
@@ -0,0 +1,43 @@
+import os
+from pathlib import Path
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+
+project_name = "end_to_end_datascience_project"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/components/data_ingestion.py",
+    f"src/{project_name}/components/data_transformation.py",
+    f"src/{project_name}/components/model_trainer.py",
+    f"src/{project_name}/components/model_monitoring.py",
+    f"src/{project_name}/pipelines/__init__.py",
+    f"src/{project_name}/pipelines/training_pipeline.py",
+    f"src/{project_name}/pipelines/prediction_pipeline.py",
+    f"src/{project_name}/exception.py",
+    f"src/{project_name}/logger.py",
+    f"src/{project_name}/utils.py",
+    "app.py",
+    "dockerfile",
+    "requirements.txt",
+    "setup.py"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Created directory: {filedir}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+        logging.info(f"Created file: {filepath}")
+    else:
+        logging.info(f"File already exists and is not empty: {filepath}")
